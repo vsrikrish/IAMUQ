9a278039e3e129d98e273bb08aea5960e05b6a8f
vsrikrish <vxs914@psu.edu>
2019-06-24 10:10:41 -0400

    allow flexible log-normal priors

--- IAMUQ/R/priors.R
+++ IAMUQ/R/priors.R
@@ -3,8 +3,7 @@
 #' \code{create_prior_list} creates a list for use in \code{\link{log_pri}}.
 #'  The parameters for each prior density function are estimated from the
 #'  confidence interval bounds in the data frame. Available (by default)
-#'  density functions are normal, uniform, and log-normal. Log-normal priors
-#'  are defined by meanlog = -1 and sdlog=1 no matter what.
+#'  density functions are normal, uniform, and log-normal.
 #'
 #' @param prior_df Data frame of information about the prior density
 #'  functions. Each row (corresponding to a single parameter) should have the
@@ -13,14 +12,13 @@
 #'  \item type, the type of prior density function. Options are "normal,"
 #'  "uniform," and "log-normal";
 #'  \item lower, numeric value defining the lower value. If the type is
-#'  "normal," this should be the 0.05 quantile (or the lower bound of the 90%
-#'  credible interval). If the type is "uniform," this should be the lower
-#'  bound of the distribution. If the log-normal, this value is ignored;
+#'  "normal" or "log-normal" this should be the 0.05 quantile (or the lower
+#'  bound of the 90% credible interval). If the type is "uniform," this
+#'  should be the lower bound of the distribution;
 #'  \item upper, numeric value defining the upper value. If the type is
-#'  "normal," this should be the 0.95 quantile (or the upper bound of the 90%
-#'  credible interval). If the type is "uniform," this should be the upper
-#'  bound of the distribution. If the log-normal, this value is ignored.
-#'  }
+#'  "normal" or "log-normal" this should be the 0.95 quantile (or the upper
+#'  bound of the 90% credible interval). If the type is "uniform," this
+#'  should be the upper bound of the distribution. }
 #' @return List with each entry corresponding to an individual parameter.
 #'  Each of these lists has the following entries: \itemize{
 #'  \item type, the parameter type;
@@ -57,8 +55,8 @@ create_prior_list <- function(prior_df) {
       priors[[name]][['dens.fun']] <- 'dlnorm'
       priors[[name]][['quant.fun']] <- 'qlnorm'
       priors[[name]][['rand.fun']] <- 'rlnorm'
-      priors[[name]][['meanlog']] <- -1
-      priors[[name]][['sdlog']] <- 1
+      priors[[name]][['meanlog']] <- mean(c(log(prior_df[i, 'lower']), log(prior_df[i, 'upper'])))
+      priors[[name]][['sdlog']] <- (log(prior_df[i, 'upper']) - log(prior_df[i, 'lower'])) / (qnorm(0.95) - qnorm(0.05))
     }
   }
   
---
+++ R/calib_priors.R
@@ -0,0 +1,116 @@
+#########################################################
+# set_prior_params(parnames):                           #
+#   This function sets up a data frame of prior         #
+#   parameters with more "intuitive" parameters such as #
+#   confidence interval bounds. Those are then          #
+#   converted to the parameters required by the density #
+#   functions when necessary.                           #
+#########################################################
+
+set_prior_params <- function(parnames) {
+  
+  # initialize prior parameter data frame
+  prior_df <- data.frame(name=parnames, type=character(length(parnames)), lower=numeric(length(parnames)), upper=numeric(length(parnames)), stringsAsFactors=FALSE)
+  
+  # loop over data frame rows and fill in values
+  for (i in 1:nrow(prior_df)) {
+    name <- prior_df[i, 'name']
+    if (grepl('sigma', name)) {
+      prior_df[i, 'type'] <- 'log-normal'
+      prior_df[i, 'lower'] <- qlnorm(0.05 -1, 1)
+      prior_df[i, 'upper'] <- qlnorm(0.95, -1, 1)
+    } else if (grepl('eps', name)) {
+      prior_df[i, 'type'] <- 'log-normal'
+      prior_df[i, 'lower'] <- qlnorm(0.05 -1, 1)
+      prior_df[i, 'upper'] <- qlnorm(0.95, -1, 1)
+    } else if (name == 'psi1') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.0001
+      prior_df[i, 'upper'] <- 0.15
+    } else if (name == 'psi2') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0
+      prior_df[i, 'upper'] <- 50
+    } else if (name == 'psi3') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 6.9
+      prior_df[i, 'upper'] <- 14.4
+    } else if (name == 'P0') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.3
+      prior_df[i, 'upper'] <- 0.9
+    } else if (name == 'lambda') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.6
+      prior_df[i, 'upper'] <- 0.8
+    } else if (name == 's') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.22
+      prior_df[i, 'upper'] <- 0.26
+    } else if (name == 'delta') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0.01
+      prior_df[i, 'upper'] <- 0.14
+    } else if (name == 'alpha') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.0007
+      prior_df[i, 'upper'] <- 0.0212
+    } else if (name == 'As') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 5.3
+      prior_df[i, 'upper'] <- 16.11
+    } else if (name == 'pi') {
+      prior_df[i, 'type'] <- 'normal'
+      prior_df[i, 'lower'] <- 0.62
+      prior_df[i, 'upper'] <- 0.66
+    } else if (name == 'A0') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0
+      prior_df[i, 'upper'] <- 3
+    } else if (name == 'rho2') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0
+      prior_df[i, 'upper'] <- 0.75
+    } else if (name == 'rho3') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0
+      prior_df[i, 'upper'] <- 0.75
+    } else if (name == 'tau2') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 1700
+      prior_df[i, 'upper'] <- 2100
+    } else if (name == 'tau3') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 1700
+      prior_df[i, 'upper'] <- 2100
+    } else if (name == 'tau4') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 2020
+      prior_df[i, 'upper'] <- 2500
+    } else if (name == 'kappa') {
+      prior_df[i, 'type'] <- 'uniform'
+      prior_df[i, 'lower'] <- 0.005
+      prior_df[i, 'upper'] <- 0.2
+    } else if (grepl('a_', name)) {
+      lab <- sub('.*_', '', name)
+      if (is.na(as.numeric(lab))) {
+        prior_df[i, 'type'] <- 'uniform'
+        prior_df[i, 'lower'] <- 0.5
+        prior_df[i, 'upper'] <- 1
+      } else {
+        idx <- as.numeric(unlist(strsplit(lab, '*')))
+        if (idx[1] == idx[2]) {
+          prior_df[i, 'type'] <- 'normal'
+          prior_df[i, 'lower'] <- 0
+          prior_df[i, 'upper'] <- 1
+        } else {
+          prior_df[i, 'type'] <- 'normal'
+          prior_df[i, 'lower'] <- -1
+          prior_df[i, 'upper'] <- 1
+        }
+      }
+    }
+  }
+  
+  prior_df
+}
