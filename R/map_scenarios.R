23a872dfd86f807b97660c7963e67f431b0a35e8
vsrikrish <vxs914@psu.edu>
2020-02-17 13:46:02 -0500

    added scripts for MAP and MCMC scenario runs

---
+++ R/map_scenarios.R
@@ -0,0 +1,99 @@
+rm(list=ls()) # clean up environment
+
+library(IAMUQ)
+source('R/calib_priors.R')
+
+## set case for this run
+# read in PBS job array index to specify type
+aid <- Sys.getenv('PBS_ARRAYID')
+# if PBS_ARRAYID doesn't exist, this should be passed as a command line argument or set interactively
+if (aid == '') {
+  if (!exists('type')) {
+    args <- commandArgs(trailingOnly=TRUE)
+    scenario <- args[1]
+  }
+} else {
+  scenarios <- c('iid', 'base', 'short', 'low', 'high', 'alt_zc')
+  id <- as.numeric(aid)
+  scenario <- scenarios[id]
+}
+
+## set model run parameters associated with each scenario
+# the base scenario corresponds to the model defaults, but we set it up here for consistency's sake
+if (scenario == 'base') {
+  data_yrs <- 1820:2014 # years for observational constraints
+  ff_thresh <- 6000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 1700:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype='var'
+} else if (scenario == 'short') {
+  data_yrs <- 1950:2014 # years for observational constraints
+  ff_thresh <- 6000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 1700:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype <- 'var' # residual structure type
+} else if (scenario == 'iid') {
+  data_yrs <- 1820:2014 # years for observational constraints
+  ff_thresh <- 6000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 1700:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype <- 'iid'
+} else if (scenario == 'low') {
+  data_yrs <- 1820:2014 # years for observational constraints
+  ff_thresh <- 3000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 2015:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype <- 'var' # residual structure type
+} else if (scenario == 'high') {
+  data_yrs <- 1820:2014 # years for observational constraints
+  ff_thresh <- 10000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 2015:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype <- 'var' # residual structure type
+} else if (scenario == 'alt_zc') {
+  data_yrs <- 1820:2014 # years for observational constraints
+  ff_thresh <- 6000 # fossil fuel constraint in GtC
+  ff_const_yrs <- 1700:2500 # years over which fossil fuel constraint is evaluated
+  exp_gwp <- FALSE # do we do probabilistic inversion for average GWP per capita?
+  exp_co2 <- TRUE # do we do probabilistic inversion for CO2 emissions in 2100?
+  residtype <- 'var' # residual structure type
+}
+
+# set parameter names based on residual structure type
+if (residtype == 'ar') {
+  parnames <- c('psi1', 'psi2', 'psi3', 'P0', 'lambda', 's', 'delta', 'alpha', 'As', 'pi', 'A0', 'rho2', 'rho3', 'tau2', 'tau3', 'tau4', 'kappa', 'sigma_pop', 'sigma_prod', 'sigma_emis', 'a_pop', 'a_prod', 'a_emis', , 'eps1_pop', 'eps1_prod', 'eps1_emis')
+} else if (residtype == 'iid') {
+  parnames <- c('psi1', 'psi2', 'psi3', 'P0', 'lambda', 's', 'delta', 'alpha', 'As', 'pi', 'A0', 'rho2', 'rho3', 'tau2', 'tau3', 'tau4', 'kappa', 'sigma_pop', 'sigma_prod', 'sigma_emis')
+} else if (residtype == 'var') {
+  parnames <- c('psi1', 'psi2', 'psi3', 'P0', 'lambda', 's', 'delta', 'alpha', 'As', 'pi', 'A0', 'rho2', 'rho3', 'tau2', 'tau3', 'tau4', 'kappa', 'sigma_pop', 'sigma_prod', 'sigma_emis', 'a_11', 'a_22', 'a_33', 'a_21', 'a_31', 'a_12', 'a_23', 'a_13', 'a_32', 'eps_pop', 'eps_prod', 'eps_emis')
+}
+
+## set up prior dataframe
+prior_df <- set_prior_params(parnames)
+# if scenario involves changing prior distributions, do so here
+if (scenario == 'alt_zc') {
+  # modify zero-carbon half-saturation year (tau_4) prior distribution
+  zc_idx <- match('tau4', prior_df[, 'name'])
+  prior_df[zc_idx, 'type'] <- 'normal'
+  prior_df[zc_idx, 'lower'] <- 2100
+  prior_df[zc_idx, 'upper'] <- 2400
+}
+
+## find MAP estimate
+map_out <- find_map(neg_log_post, parnames=parnames, residtype=residtype, prior_df=prior_df, data_yrs=data_yrs, NP_scale=25, n_iter=5000, parallel=TRUE, trace=FALSE, thresh=ff_thresh, ff_const_yrs=ff_const_yrs, exp_gwp=exp_gwp, exp_co2=exp_co2)
+
+## save estimate
+## save estimate
+appendix <- ''
+if (exp_gwp) {
+  appendix <- paste0(appendix, '-gwp')
+}
+if (exp_co2) {
+  appendix <- paste0(appendix, '-co2')
+}
+saveRDS(map_out, paste0('output/map_', scenario, appendix, '.rds'))
